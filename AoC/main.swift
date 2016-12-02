3c87dcf222a584a1b7b3faf2c40ad19e7a166f91
Jan Karlbom <jankarlbom@macbook-pro.local>
2016-12-02 18:50:37 +0100

    Initial Commit

---
+++ AoC/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AoC
+//
+//  Created by Jan Karlbom on 2016-12-02.
+//  Copyright © 2016 Jan Karlbom. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
